647563207f633dac384d991baf41a78dbf2d9f89
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-08-09 14:38:51 +0100

    Imported Tailwind & updated basic file structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header() {
+
+  return (
+    <nav>
+        <h1>Where in the world?</h1>
+        <button class="flex">
+            <img src="src/assets/light-mode-icon.png"/>
+            <p>Dark mode</p>
+        </button>
+    </nav>
+  )
+}
+
+export default Header
