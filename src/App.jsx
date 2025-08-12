74d7468daffd521ed9c90360b895abdcff6abb86
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-08-12 17:41:30 +0100

    Navbar component & styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import Header from './components/Header'
 export default function App() {
 
   return (
-    <div>
+    <>
       <Header />
-      <br/>
-      <a href="https://www.flaticon.com/free-icons/dark" title="dark icons">Dark icons created by adriansyah - Flaticon</a>
-    </div>
+    </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
-
 function Header() {
-
   return (
-    <nav>
-        <h1>Where in the world?</h1>
-        <button class="flex">
-            <img src="src/assets/light-mode-icon.png"/>
-            <p>Dark mode</p>
-        </button>
+    <nav className="flex justify-between items-center w-full h-24 px-20 lg:px-30 shadow-md">
+      <h1 className="font-bold text-2xl hover:cursor-pointer">Where in the world?</h1>
+      <button className="flex items-center gap-2 hover:text-[#494949] cursor-pointer">
+        <img className="h-5 w-5 object-scale-down" src="src/assets/light-mode-icon.png" alt="Light mode" />
+        <p className="font-semibold text-lg">Dark Mode</p>
+      </button>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
