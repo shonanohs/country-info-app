ecd21efae5478a6222c125d255c1f50d222e1ecb
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-08-17 19:58:57 +0100

    Minor layout updates

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 export default function CountryInfo({ country, countries }) {
     const infoCategories = [
     { label: "Native Name", value: country.name.nativeName ? Object.values(country.name.nativeName)[0].common : country.name.common },
-    { label: "Population", value: country.population.toLocaleString() },
     { label: "Region", value: country.region },
-    { label: "Sub Region", value: country.subregion },
     { label: "Capital", value: country.capital?.[0] },
-    { label: "Currencies", value: country.currencies ? Object.values(country.currencies).map(c => c.name).join(", ") : "" },
-    { label: "Languages", value: country.languages ? Object.values(country.languages).join(", ") : "" }
+    { label: "Sub Region", value: country.subregion },
+    { label: "Languages", value: country.languages ? Object.values(country.languages).join(", ") : "" },
+    { label: "Population", value: country.population.toLocaleString() },
+    { label: "Currencies", value: country.currencies ? Object.values(country.currencies).map(c => c.name).join(", ") : "" }
     ]
 
     const borderCountries = country.borders || [];
@@ -39,36 +39,36 @@ export default function CountryInfo({ country, countries }) {
         </Link>
 
         <div className="flex mt-10">
-        <img className="w-full max-w-lg" src={country.flags.svg} alt={`${country.name.common} flag`} />
-        <div className="ml-30  max-w-200">
-            <h1 className="font-bold text-2xl dark:text-white">{country.name.common}</h1>
+            <img className="w-full max-w-lg" src={country.flags.svg} alt={`${country.name.common} flag`} />
+            <div className="ml-30  min-w-250 max-w-250">
+                <h1 className="font-bold text-2xl dark:text-white">{country.name.common}</h1>
 
-            <div className="grid grid-cols-2 gap-y-2 gap-x-10 mt-10 mb-20 max-w-150">
-            {infoCategories.map((item) => (
-                <p key={item.label} className="font-semibold dark:text-white">
-                {item.label}: <span className="font-normal">{item.value}</span>
-                </p>
-            ))}
-            </div>
+                <div className="grid grid-cols-2 gap-y-2 gap-x-30 mt-10 mb-20 max-w-150">
+                {infoCategories.map((item) => (
+                    <p key={item.label} className="font-semibold dark:text-white">
+                    {item.label}: <span className="font-normal">{item.value}</span>
+                    </p>
+                ))}
+                </div>
 
-            <div className="flex flex-wrap gap-3">
-                <p className="font-semibold dark:text-white">Border Countries:</p>
-                {borderCountryNames.length > 0 ? (
-                    borderCountryNames.map((border) => (
-                    <Link key={border} to={`/country/${border}`}>
-                        <button
-                            key={border}
-                            className="hover:cursor-pointer dark:text-white dark:bg-[#2d3742] bg-white rounded-sm shadow-md px-5 py-1"
-                        >
-                            {border}
-                        </button>
-                    </Link>
-                    ))
-                ) : (
-                    <span className="dark:text-white">None</span>
-                )}
+                <div className="flex flex-wrap gap-3">
+                    <p className="font-semibold dark:text-white">Border Countries:</p>
+                    {borderCountryNames.length > 0 ? (
+                        borderCountryNames.map((border) => (
+                        <Link key={border} to={`/country/${border}`}>
+                            <button
+                                key={border}
+                                className="hover:cursor-pointer dark:text-white dark:bg-[#2d3742] bg-white rounded-sm shadow-md px-5 py-1"
+                            >
+                                {border}
+                            </button>
+                        </Link>
+                        ))
+                    ) : (
+                        <span className="dark:text-white">None</span>
+                    )}
+                </div>
             </div>
-        </div>
         </div>
     </div>
     )
