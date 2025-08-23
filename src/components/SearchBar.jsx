73433fbb53eb203b5266962d7a00aed97eba0b25
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-08-23 15:02:17 +0100

    Minor layout changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,19 +14,19 @@ export default function SearchBar({ setFilteredCountries, countries }) {
     <div className="flex relative md:pb-0 pb-5">
       <img
         src={search}
-        className="z-10 absolute left-10 top-4 h-6 pointer-events-none block dark:hidden"
+        className="z-10 absolute left-6 top-4 h-6 pointer-events-none block dark:hidden"
         alt="search"
       />
       <img
         src={searchDark}
-        className="z-10 absolute left-10 top-4 h-6 pointer-events-none hidden dark:block"
+        className="z-10 absolute left-6 top-4 h-6 pointer-events-none hidden dark:block"
         alt="search"
       />
       <input
         type="text"
         placeholder="Search for a country..."
         onChange={handleSearch}
-        className="bg-white dark:bg-[#2d3742] dark:text-white rounded-sm shadow-md pl-20 pr-40 py-4 z-0 focus:outline-0"
+        className="bg-white dark:bg-[#2d3742] dark:text-white rounded-sm shadow-md pl-15 pr-33 py-4 z-0 focus:outline-0"
       />
     </div>
   )
