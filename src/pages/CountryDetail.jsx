9a63a7ff7adb6a4da0815e2bfdd7faa2e44452e9
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-08-23 15:29:48 +0100

    'Not found' bug fix

--- src/pages/CountryDetail.jsx
+++ src/pages/CountryDetail.jsx
@@ -1,22 +1,38 @@
 import CountryInfo from "../components/CountryInfo";
 import Header from "../components/Header";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function CountryDetail({ countries }) {
     const { countryName } = useParams();
+    const [showNotFound, setShowNotFound] = useState(false);
 
     const country = countries.find(
         (c) => c.name.common.toLowerCase() === countryName.toLowerCase()
     );
 
-    if (!country) return (
-    <>
+
+    useEffect(() => {
+    if (!country) {
+        const timer = setTimeout(() => setShowNotFound(true), 250)
+        return () => clearTimeout(timer)
+    } else {
+        setShowNotFound(false)
+    }
+    }, [country])
+
+    if (!country && showNotFound) {
+    return (
+        <>
         <Header />
         <div className="flex items-center justify-center h-[calc(100vh-6rem)]">
-        <p className="dark:text-white text-xl">Country not found</p>
+            <p className="dark:text-white text-xl">Country not found</p>
         </div>
-    </>
+        </>
     )
+    }
+
+    if (!country) return <Header />
 
     return (
         <>
